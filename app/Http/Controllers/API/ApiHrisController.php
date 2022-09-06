aa026fe486a29b20a6390f65738b8070afcbcf8f
bagus <bagus.oetomo11@gmail.com>
2022-09-06 11:08:08 +0700

    update api for store attendace

--- app/Http/Controllers/API/ApiHrisController.php
+++ app/Http/Controllers/API/ApiHrisController.php
@@ -74,8 +74,72 @@ class ApiHrisController extends Controller
             );
         }
     }
-
+    // port 03
     public function storeAttendanceKarawang(){
+        $zk = new ZKTeco('192.168.10.3', 4370, 'TCP');
+        
+        if ($zk->connect())
+        {
+            $attendance = $zk->getAttendance();
+            $array_post=[];
+            foreach($attendance as $item)
+            {
+            // Sistem akan memvalidasi, jika data ada, maka data tersebut tidak di insert
+            $validasi_1 = HrisAttendance::where('attend_date', $item['timestamp'])->where('created_by', $item['id'])->count();
+                if($validasi_1 == 0 ){
+                    $created_at = strtotime($item['timestamp']);
+                    $formatDate = date('YmdHis',$created_at);
+                    $array =[
+                        'attendanceid'=>$item['uid'],
+                        'attenddata'=>$item['id'].$formatDate,
+                        'attend_date'=>$item['timestamp'],
+                        'day'=>date('d',$created_at),
+                        'month'=>date('m',$created_at),
+                        'year'=>date('Y',$created_at),
+                        'hour'=>date('H',$created_at),
+                        'minute'=>date('i',$created_at),
+                        'second'=>date('s',$created_at),
+                        'status'=>$item['type'],
+                        'machineno'=>0,
+                        'machine_code'=>'FINGERPRINT',
+                        'uploadstatus'=>1,
+                        'company_id'=>1,
+                        'remark'=>1,
+                        'photo'=>'',
+                        'geolocation'=>'KARAWANG',
+                        'att_on'=>1,
+                        'created_by'=>$item['id'],
+                        'modified_by'=>$item['id'],
+                        'modified_date'=>date('Y-m-d H:i:s'),
+                        'created_date'=>date('Y-m-d H:i:s'),
+                    ];
+                    array_push($array_post,$array);
+                }
+            }
+            if($array_post != NULL) {
+                $insert = HrisAttendance::insertOrIgnore($array_post);
+                return Helper::success(
+                    $insert,
+                    'Data berhasil di-import ke Database'
+                );
+            } else {
+                return Helper::error(
+                    NULL,
+                    'Data gagal diimport ke Database',
+                    404
+                );
+            }
+        } else {
+            return Helper::error(
+                NULL,
+                'Gagal untuk koneksi ke IP',
+                500
+            );
+        }
+    }
+// Karawang
+    // port 04
+    public function storeAttendanceKarawang2(){
         $zk = new ZKTeco('192.168.10.4', 4370, 'TCP');
         
         if ($zk->connect())
@@ -137,6 +201,135 @@ class ApiHrisController extends Controller
             );
         }
     }
+// T8
+        public function storeAttendanceT8(){
+            $zk = new ZKTeco('192.168.1.200', 4370, 'TCP');
+            
+            if ($zk->connect())
+            {
+                $attendance = $zk->getAttendance();
+                $array_post=[];
+                foreach($attendance as $item)
+                {
+                // Sistem akan memvalidasi, jika data ada, maka data tersebut tidak di insert
+                $validasi_1 = HrisAttendance::where('attend_date', $item['timestamp'])->where('created_by', $item['id'])->count();
+                    if($validasi_1 == 0 ){
+                        $created_at = strtotime($item['timestamp']);
+                        $formatDate = date('YmdHis',$created_at);
+                        $array =[
+                            'attendanceid'=>$item['uid'],
+                            'attenddata'=>$item['id'].$formatDate,
+                            'attend_date'=>$item['timestamp'],
+                            'day'=>date('d',$created_at),
+                            'month'=>date('m',$created_at),
+                            'year'=>date('Y',$created_at),
+                            'hour'=>date('H',$created_at),
+                            'minute'=>date('i',$created_at),
+                            'second'=>date('s',$created_at),
+                            'status'=>$item['type'],
+                            'machineno'=>0,
+                            'machine_code'=>'FINGERPRINT',
+                            'uploadstatus'=>1,
+                            'company_id'=>1,
+                            'remark'=>1,
+                            'photo'=>'',
+                            'geolocation'=>'T8',
+                            'att_on'=>1,
+                            'created_by'=>$item['id'],
+                            'modified_by'=>$item['id'],
+                            'modified_date'=>date('Y-m-d H:i:s'),
+                            'created_date'=>date('Y-m-d H:i:s'),
+                        ];
+                        array_push($array_post,$array);
+                    }
+                }
+                if($array_post != NULL) {
+                    $insert = HrisAttendance::insertOrIgnore($array_post);
+                    return Helper::success(
+                        $insert,
+                        'Data berhasil di-import ke Database'
+                    );
+                } else {
+                    return Helper::error(
+                        NULL,
+                        'Data gagal diimport ke Database',
+                        404
+                    );
+                }
+            } else {
+                return Helper::error(
+                    NULL,
+                    'Gagal untuk koneksi ke IP',
+                    500
+                );
+            }
+        }
+// HO
+        public function storeAttendanceHO(){
+            $zk = new ZKTeco('192.168.1.19', 4370, 'TCP');
+            
+            if ($zk->connect())
+            {
+                $attendance = $zk->getAttendance();
+                $array_post=[];
+                foreach($attendance as $item)
+                {
+                // Sistem akan memvalidasi, jika data ada, maka data tersebut tidak di insert
+                $validasi_1 = HrisAttendance::where('attend_date', $item['timestamp'])->where('created_by', $item['id'])->count();
+                    if($validasi_1 == 0 ){
+                        $created_at = strtotime($item['timestamp']);
+                        $formatDate = date('YmdHis',$created_at);
+                        $array =[
+                            'attendanceid'=>$item['uid'],
+                            'attenddata'=>$item['id'].$formatDate,
+                            'attend_date'=>$item['timestamp'],
+                            'day'=>date('d',$created_at),
+                            'month'=>date('m',$created_at),
+                            'year'=>date('Y',$created_at),
+                            'hour'=>date('H',$created_at),
+                            'minute'=>date('i',$created_at),
+                            'second'=>date('s',$created_at),
+                            'status'=>$item['type'],
+                            'machineno'=>0,
+                            'machine_code'=>'FINGERPRINT',
+                            'uploadstatus'=>1,
+                            'company_id'=>1,
+                            'remark'=>1,
+                            'photo'=>'',
+                            'geolocation'=>'HO',
+                            'att_on'=>1,
+                            'created_by'=>$item['id'],
+                            'modified_by'=>$item['id'],
+                            'modified_date'=>date('Y-m-d H:i:s'),
+                            'created_date'=>date('Y-m-d H:i:s'),
+                        ];
+                        array_push($array_post,$array);
+                    }
+                }
+                if($array_post != NULL) {
+                    $insert = HrisAttendance::insertOrIgnore($array_post);
+                    return Helper::success(
+                        $insert,
+                        'Data berhasil di-import ke Database'
+                    );
+                } else {
+                    return Helper::error(
+                        NULL,
+                        'Data gagal diimport ke Database',
+                        404
+                    );
+                }
+            } else {
+                return Helper::error(
+                    NULL,
+                    'Gagal untuk koneksi ke IP',
+                    500
+                );
+            }
+}
+// Cimanggis
+
+
     public function all()
     {
         $get_data = HrisAttendance::all();
--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::get('getAttendance', [ApiHrisController::class, 'getAttendance'])->name('getAttendance');
 Route::post('storeAttendanceKarawang', [ApiHrisController::class, 'storeAttendanceKarawang'])->name('storeAttendanceKarawang');
+Route::post('storeAttendanceKarawang2', [ApiHrisController::class, 'storeAttendanceKarawang2'])->name('storeAttendanceKarawang2');
+Route::post('storeAttendanceHO', [ApiHrisController::class, 'storeAttendanceHO'])->name('storeAttendanceHO');
+Route::post('storeAttendanceT8', [ApiHrisController::class, 'storeAttendanceT8'])->name('storeAttendanceT8');
 Route::get('all', [ApiHrisController::class, 'all'])->name('all');
